ae30412757c90f26343319d77bad06fd282aa16d
kamalDobhalGit <kamaldobhal1995@gmail.com>
2024-08-13 21:06:26 +0530

    removing build

--- build/routes/v1/user.routes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const userRouter = (0, express_1.Router)();
-userRouter.get("/", (req, res) => res.send("This is a user route"));
-exports.default = userRouter;
